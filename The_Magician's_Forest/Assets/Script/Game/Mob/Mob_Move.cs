04ed76dfe0a8684cbb2044e1fcd4651025a35ed5
park0girin <park0girin@yuhan.ac.kr>
2024-05-08 09:13:54 +0900

    이름변경

---
+++ The_Magician's_Forest/Assets/Script/Game/Mob/Mob_Move.cs
@@ -0,0 +1,236 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Mob_Move : MonoBehaviour
+{
+    public GameManager GameManager;
+    public mobType mob;
+    public float speed_input;
+    float speed;
+    public float skillTime;
+    public float hp_input;
+    public float hp;
+    bool specialSkill;
+    bool cannotBeHit;
+    sprite_change ChangeScript;
+
+    public enum mobType
+    {
+        Red,
+        Green,
+        Blue,
+        White,
+        BOSS
+    }
+    float Ran(float max, float min)
+    {
+        float value = Random.Range(max, min);
+        return value;
+    }
+    private void OnEnable()
+    {
+        GameManager = FindObjectOfType<GameManager>();
+        hp = hp_input;
+        hp *= GameManager.HpMultiplier;
+        speed = Ran(speed_input + 1, speed_input - 1);
+        specialSkill = false;
+        skillTime = 0;
+        if (mob == mobType.White)
+        {
+            ChangeScript = GetComponent<sprite_change>();
+        }
+    }
+    void Green()
+    {
+        cannotBeHit = false;
+        if (!specialSkill)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        else if(specialSkill)
+        {
+            skillTime += Time.deltaTime;
+            if (skillTime > 2f)
+            {
+                cannotBeHit = false;
+                specialSkill =! specialSkill;
+            }
+        }
+    }
+    void Red()
+    {
+        if (!specialSkill)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+        if (transform.position.y < 4)
+        {
+            skillTime += Time.deltaTime;
+            if (skillTime > Ran(5, 2))
+            {
+                specialSkill = true;
+            }
+            if (specialSkill)
+            {
+                Vector2 pos = transform.position;
+                GameManager.Boom(pos);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    void Blue()
+    {
+        float minX = -2.5f;
+        float maxX = 2.5f;
+        float minY = -4f;
+        float maxY = 4f;
+
+        if (transform.position.y < maxY && transform.position.y > minY)
+        {
+            float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+            transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+
+            if (!specialSkill)
+            {
+                skillTime += Time.deltaTime;
+                if (skillTime >= 1f)
+                {
+                    transform.Rotate(0, 0, Ran(360f, 0f));
+                    specialSkill = true;
+                }
+            }
+            else
+            {
+                transform.Rotate(Vector2.zero);
+                skillTime -= Time.deltaTime;
+                if (skillTime <= 0.9f)
+                {
+                    skillTime = 0;
+                    specialSkill = false;
+                }
+                else
+                {
+                    transform.Translate(Vector2.down * speed * 5f * Time.deltaTime);
+                }
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.identity;
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+    void White()
+    {
+        if (!specialSkill)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+            if (hp <= (hp_input / 2))
+            {
+                specialSkill = true;
+            }
+        }
+        if (specialSkill)
+        {
+            ChangeScript.Change();
+            transform.Translate(Vector2.down * speed * 10 * Time.deltaTime);
+        }
+    }
+    void BOSS()
+    {
+        if (transform.position.y >= 3)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }
+    }
+    private void Update()
+    {
+        if (GameManager.GameOver)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (hp <= 0)
+        {
+            specialSkill = false;
+            Vector2 pos = transform.position;
+            if (mob == mobType.BOSS)
+            {
+                GameManager.BossBattle = false;
+                GameManager.Level++;
+                GameManager.Gamecontinue();
+            }
+            else
+            {
+                GameManager.MobCount++;
+                GameManager.MakeBuff(pos);
+            }
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            if (!GameManager.Freeze)
+            {
+                switch (mob)
+                {
+                    case mobType.Green:
+                        Green();
+                        break;
+                    case mobType.Red:
+                        Red();
+                        break;
+                    case mobType.Blue:
+                        Blue();
+                        break;
+                    case mobType.White:
+                        White();
+                        break;
+                    case mobType.BOSS:
+                        BOSS();
+                        break;
+                }
+                if (GameManager.BossBattle && mob != mobType.BOSS)
+                {
+                    gameObject.SetActive(false);
+                    return;
+                }
+            }
+            else
+            {
+                GameManager.FreezeSkillTime += Time.deltaTime;
+                if(GameManager.FreezeSkillTime > 2.0f)
+                {
+                    GameManager.Freeze = false;
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(mob == mobType.Green && collision.CompareTag("bullet"))
+        {
+            specialSkill = true;
+        }
+        if (mob == mobType.Red && transform.position.y < 4)
+        {
+            specialSkill = true;
+        }
+        if (mob == mobType.Blue && collision.CompareTag("bullet"))
+        {
+            hp = 0;
+        }
+        if (collision.CompareTag("bullet"))
+        {
+            if (!GameManager.Penetration)
+            {
+                collision.gameObject.SetActive(false);
+            }
+            if (!cannotBeHit && !(mob == mobType.BOSS && transform.position.y > 4))
+            {
+                hp -= GameManager.BulletDamage;
+            }
+        }
+
+    }
+}
---
+++ The_Magician's_Forest/Assets/Script/Game/Player/Player_shot.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_shot : MonoBehaviour
+{
+    public GameManager GameManager;
+    public GameObject bulletobj;
+    public int poolSize = 10;
+    GameObject[] bulletPool;
+    public GameObject pos;
+    public float angle;
+    float shotTime;
+    bool skilling;
+    int bulletsToShoot = 3; // ShotType이 0일 때 발사할 총알 개수
+
+    void Start()
+    {
+        GameManager = FindObjectOfType<GameManager>(); // GameManager 찾기
+        bulletPool = new GameObject[poolSize];
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject bullet = Instantiate(bulletobj);
+            bulletPool[i] = bullet;
+            bullet.SetActive(false);
+        }
+    }
+
+    // 총알 발사 메서드
+    void ShootBullets(float startingAngle)
+    {
+        float intervalAngle = angle; // 각 총알 사이의 각도 간격
+
+        for (int i = 0; i < bulletsToShoot; i++)
+        {
+            GameObject bullet = GetNextInactiveBullet(); // 비활성화된 총알 가져오기
+            if (bullet != null)
+            {
+                // 각 총알의 발사 각도 계산
+                float bulletAngle = startingAngle + i * intervalAngle;
+                ShootBullet(bullet, bulletAngle);
+            }
+        }
+    }
+
+    // 다음 비활성화된 총알 가져오기
+    GameObject GetNextInactiveBullet()
+    {
+        foreach (GameObject bullet in bulletPool)
+        {
+            if (!bullet.activeSelf)
+            {
+                return bullet;
+            }
+        }
+        return null;
+    }
+
+    // 총알 발사 메서드
+    void ShootBullet(GameObject bullet, float angle)
+    {
+        // 각도를 라디안으로 변환하여 총알의 방향 벡터 계산
+        Vector3 direction = Quaternion.Euler(0, 0, angle) * Vector3.up;
+
+        // 총알의 방향과 위치 설정
+        bullet.transform.position = pos.transform.position; // 발사 위치
+        bullet.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction); // 발사 각도
+        bullet.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (!GameManager.GameOver)
+        {
+            if (!skilling)
+            {
+                shotTime += Time.deltaTime;
+                if (shotTime >= ScenesManager.Instance.ShotInterval)
+                {
+                    // ShotType이 0일 때만 3개의 총알을 발사
+                    if (ScenesManager.Instance.ShotType == 0)
+                    {
+                        // 첫 번째 총알의 시작 각도 설정
+                        float startingAngle = -angle; // 첫 번째 총알은 -angle 각도로 발사
+
+                        ShootBullets(startingAngle); // 총알들을 발사하고 각 총알의 각도를 설정
+
+                        shotTime = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public void SkillShot()
+    {
+        // 모든 활성화된 총알을 비활성화
+        DeactivateAllBullets();
+
+        // 첫 번째 총알의 시작 x 위치
+        float startX = -2.5f;
+        // 일정 간격 계산
+        float interval = CalculateBulletInterval();
+
+        // 총알 발사
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject bullet = GetNextInactiveBullet(); // 비활성화된 총알 가져오기
+            if (bullet != null)
+            {
+                // 총알 위치 설정 (x는 일정 간격으로 배치, y는 고정된 값으로 설정)
+                float posX = startX + i * interval;
+                float posY = -4.0f;
+
+                // 총알 발사 각도 설정 (위쪽 방향으로 발사)
+                float bulletAngle = 0.0f;
+
+                // 총알 활성화 및 위치/각도 설정
+                bullet.SetActive(true);
+                bullet.transform.position = new Vector3(posX, posY, 0f);
+                bullet.transform.rotation = Quaternion.Euler(0f, 0f, bulletAngle);
+            }
+        }
+    }
+
+
+    // 모든 활성화된 총알을 비활성화하는 메서드
+    void DeactivateAllBullets()
+    {
+        foreach (GameObject bullet in bulletPool)
+        {
+            if (bullet.activeSelf)
+            {
+                bullet.SetActive(false);
+            }
+        }
+    }
+
+    // 일정 간격 계산
+    float CalculateBulletInterval()
+    {
+        return (5.0f / (poolSize - 1)); // -2.5에서 2.5까지의 간격을 poolSize-1 개로 나눈 값
+    }
+}
